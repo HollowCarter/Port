644128e0d8705c393268ce70677d303c10969772
HollowCarter <leobourke2001@gmail.com>
2021-06-15 02:23:14 +0000

    Fixed VOOOM

--- miner.js
+++ miner.js
@@ -4,25 +4,26 @@ canvas.height = window.innerHeight
 
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#008000"; 
-ctx.fillRect(0, 0, canvas.width, canvas.height); 
-
+drawBack();
 drawMiner();
 
-function drawMiner() {
-    ctx.fillStyle = "#804000"; 
-    ctx.fillRect(0, 0, 100, 50); 
-    ctx.fillStyle = "#7f4f1f"; 
-    ctx.fillRect(0, 50, 100, 25); 
-}
-
 var MinerX=0;
+var MinerY=0;
 var l=false;
 var Gstart=false;
 
 window.addEventListener("keydown", function(event){
     if (event.key == "d"){
-        l=true
+        MinerX=MinerX+5
+    }
+    if (event.key == "a"){
+        MinerX=MinerX-5
+    }
+    if (event.key == "s"){
+        MinerY=MinerY+5
+    }
+    if (event.key == "w"){
+        MinerY=MinerY-5
     }
     if(!Gstart){
         Gstart = true;
@@ -30,21 +31,31 @@ window.addEventListener("keydown", function(event){
     }
 });
 
-window.addEventListener("onkeyup",function(event){
-    if (event.key == "d"){
-        l=false
-    }
-})
-
 function animate(){
     if (!Gstart) return;
-    if (l=true){MinerX=MinerX+1}
     draw();
     window.requestAnimationFrame(animate);
 }
+
 function draw(){
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    ctx.translate(MinerX,0)
+    drawBack();
+    ctx.translate(MinerX,MinerY)
     drawMiner();
+    ctx.translate(-MinerX,-MinerY)
     ctx.restore();
+}
+
+//Objects
+
+function drawBack(){
+    ctx.fillStyle = "#008000"; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+}
+
+function drawMiner() {
+    ctx.fillStyle = "#804000"; 
+    ctx.fillRect(0, 0, 100, 50); 
+    ctx.fillStyle = "#7f4f1f"; 
+    ctx.fillRect(0, 50, 100, 25); 
 }
